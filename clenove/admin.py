290d185c75f7edaab5fbdd2e649f2e4c51d7fb31
Kamran Beybutov <kamran.beybutovv@gmail.com>
2022-05-04 18:45:33 +0200

    second

--- clenove/admin.py
+++ clenove/admin.py
@@ -2,9 +2,13 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Skaut
+from .models import Skaut, Oddil
 class SkautAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug" : ("prezdivka",) } 
     list_display = ["jmeno", "prezdivka", "splneno", "vek"]
     list_filter =  ["splneno", "vek"]
+    
+    class Oddiladmin(admin.ModelAdmin):
+        list_display = ["jmeno", "heslo"]
 admin.site.register(Skaut, SkautAdmin)
+admin.site.register(Oddil)
--- clenove/models.py
+++ clenove/models.py
@@ -3,6 +3,15 @@ from django.utils.text import slugify
 from django.urls import reverse
 # Create your models here.
 
+class Oddil(models.Model):
+    jmeno = models.CharField("Jmeno", max_length=100)
+    heslo = models.CharField(max_length=300)
+
+
+    def __str__(self):
+       return self.jmeno
+   
+
 class Skaut(models.Model):
     jmeno = models.CharField("Jméno", max_length=100)
     prezdivka = models.CharField("Přezdívka",
@@ -11,7 +20,7 @@ class Skaut(models.Model):
     vek = models.IntegerField("Věk")
     slug = models.SlugField()
     splneno = models.BooleanField("Splněno", default=False, help_text="Už splnil skautskou zkoušku?")
-
+    oddil = models.ForeignKey(Oddil, verbose_name="Oddil", on_delete=models.CASCADE, null=True)
     def get_absolute_url(self):
         return reverse("detail_clena", args = [self.slug])
     
